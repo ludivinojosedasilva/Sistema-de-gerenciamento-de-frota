23875b7c66aa0929a2b7d8183c0a593234c01384
Ludivino José Da Silva <ludivinojosedasilva@gmail.com>
2025-05-14 17:04:03 -0300

    Add files via upload

--- Route.cpp
+++ Route.cpp
@@ -1,25 +1,16 @@
 #include "Route.hpp"
 
-Route::Route(string ori, string dest, float km) {
-    m_origin = new string(ori);
-    m_destination = new string(dest);
-    m_distanceKm = new float(km);
+Route::Route(string ori, string dest, float km)
+    : m_origin(ori), m_destination(dest), m_distanceKm(km) {}
+
+string Route::GetOrigin() const {
+    return m_origin;
 }
 
-Route::~Route() {
-    delete m_origin;
-    delete m_destination;
-    delete m_distanceKm;
+string Route::GetDestination() const {
+    return m_destination;
 }
 
-string Route::GetOrigin() {
-    return *m_origin;
-}
-
-string Route::GetDestination() {
-    return *m_destination;
-}
-
-float Route::GetDistanceKM() {
-    return *m_distanceKm;
+float Route::GetDistanceKM() const {
+    return m_distanceKm;
 }
